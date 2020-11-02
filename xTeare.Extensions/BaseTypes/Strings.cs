bcfb666a565989f74565910d81c0963ae7cb654a
xTeare <jansse.christian1995@gmail.com>
2020-11-02 19:04:09 +0100

    Add project files.

---
+++ xTeare.Extensions/BaseTypes/Strings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xTeare.Extensions.BaseTypes
+{
+    public static class Strings
+    {
+        public static string Between(this string Original, string A, string B, bool RemoveWhiteSpaces = true, bool IgnoreCase = false)
+        {
+            int num1 = Original.IndexOf(A, (IgnoreCase)?StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+            int num2 = Original.LastIndexOf(B, (IgnoreCase) ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+
+            if (num1 == -1)
+                return "";
+
+            if (num2 == -1)
+                return "";
+
+            int adjustedPosA = num1 + A.Length;
+
+            if (adjustedPosA >= num2)
+                return "";
+
+            string ret = Original.Substring(adjustedPosA, num2 - adjustedPosA);
+
+            if (RemoveWhiteSpaces &&char.IsWhiteSpace(ret[0]))
+                ret = ret.Substring(1);
+
+            if (RemoveWhiteSpaces && char.IsWhiteSpace(ret[ret.Length - 1]))
+                ret = ret.Substring(0, ret.Length - 1);
+
+            return ret;
+        }
+    }
+}
---
+++ xTeare.Extensions/Utils/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xTeare.Extensions.Utils
+{
+    public static class Utils
+    {
+        public static WeightedKeyPairValue RandomElementByWeight(this IList<WeightedKeyPairValue> list)
+        {
+            if (list.Count < 1)
+                return null;
+
+            int sum = list.Sum(e => e.InitialWeight);
+            int start = 0;
+            int choice = list[0].Random.Next(sum);
+
+            foreach (WeightedKeyPairValue wkvp in list)
+            {
+                for (int i = start; i < wkvp.InitialWeight + start; i++)
+                    if (i >= choice)
+                        return wkvp;
+
+                start += wkvp.InitialWeight;
+            }
+
+            return null;
+        }
+    }
+
+    public class WeightedKeyPairValue
+    {
+        public dynamic Key;
+        public dynamic Value;
+        public int InitialWeight;
+        public int WeightIncrease;
+        public int CurrentWeight { get; internal set; }
+
+        private Random random;
+        internal Random Random
+        {
+            get
+            {
+                if (random == null)
+                    random = new Random();
+                return random;
+            }
+        }
+
+        public WeightedKeyPairValue(dynamic Key, dynamic Value, int InitialWeight, int WeightIncrease)
+        {
+            this.Key = Key;
+            this.Value = Value;
+            this.InitialWeight = InitialWeight;
+            this.WeightIncrease = WeightIncrease;
+        }
+    }
+}
